00cbf13df88a3085d415df0353400906161381f3
natalia-coelho <nataliacoelho199@gmail.com>
2024-06-11 21:31:51 -0300

    improving code legibility

--- src/flows.py
+++ src/flows.py
@@ -7,12 +7,14 @@ import logging
 
 from sqlalchemy import engine_from_config
 
-logging.basicConfig(level=logging.INFO)
+logging.basicConfig(level=logging.ERROR)
+
+API_BASE_URL = "https://rickandmortyapi.com/api"
 
 @task(retries=3, retry_delay_seconds=10, timeout_seconds=0.1)
 def fetch_characters():
     try: 
-        response = requests.get('https://rickandmortyapi.com/api/character', timeout=0.1)
+        response = requests.get(f'{API_BASE_URL}/character', timeout=0.1)
         response.raise_for_status()
         return response.json()
     except requests.exceptions.Timeout:
@@ -23,15 +25,9 @@ def fetch_characters():
         raise 
 
 @task(retries=3, retry_delay_seconds=10, timeout_seconds=5)
-def get_characters():
-    response = requests.get('https://rickandmortyapi.com/api/character')
-    characters = response.json()['results']
-    return [character['name'] for character in characters]
-
-@task(retries=3, retry_delay_seconds=10, timeout_seconds=5)
 def fetch_locations():
     try:
-        response = requests.get('https://rickandmortyapi.com/api/location', timeout=5)
+        response = requests.get(f'{API_BASE_URL}/location', timeout=5)
         response.raise_for_status()
         return response.json()
     except requests.exceptions.Timeout:
@@ -40,14 +36,28 @@ def fetch_locations():
     except requests.exceptions.RequestException as e:
         logging.error(f"Erro ao buscar as localizações: {e}")
         raise FAILURE(f"Erro ao buscar as localizações: {e}")
+    
+@task(retries=3, retry_delay_seconds=10, timeout_seconds=5)
+def get_character_by_id(character_id):
+    try:
+        response = requests.get(f'{API_BASE_URL}/character/{character_id}')
+        response.raise_for_status()
+        return response.json()
+    
+    except requests.exceptions.Timeout:
+        logging.error("Timeout ao buscar o personagem.")
+    except requests.exceptions.RequestException as e:
+        logging.error(f"Erro ao buscar o personagem: {e}")
+        raise FAILURE(f"Erro ao buscar o personagem: {e}")
 
 @flow
 def rick_and_morty_flow():
     characters = fetch_characters()
     locations = fetch_locations()
-    names = get_characters()
-    logging.info(f"Quantidade de personagens encontrados: {len(characters['results'])}")
-    logging.info(f"Localizações encontradas: {len(locations['results'])}")
+    character = get_character_by_id(1)
+    print(f"Quantidade de personagens encontrados 👥: {len(characters['results'])}")
+    print(f"Localizações encontradas 🗺️: {len(locations['results'])}")
+    print(f"Nome do personagem com ID 1: {character['name']}")
     
 if __name__ == "__main__":
     rick_and_morty_flow()
